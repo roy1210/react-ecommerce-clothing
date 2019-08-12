4b476a449ec29d93dbbfd8b664a0ba988ca8febb
roy1210 <rswkure@gmail.com>
2019-08-12 16:35:43 +0800

    heroku settings

--- src/components/collectionsOverview/CollectionsOverview.js
+++ src/components/collectionsOverview/CollectionsOverview.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { createStructuredSelector } from 'reselect';
 import { selectCollectionsForPreview } from '../../redux/shop/shop.selector';
-import CollectionPreview from '../collectionPreview/CollectionPreview';
+import CollectionPreview from '../collectionPreview/CollectionPreview.js';
 import './CollectionsOverview.scss';
 
 const CollectionsOverview = ({ collections }) => {
